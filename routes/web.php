9e241047eb2f30f42b706564a9333f3267af4032
Yi Feng <fyfighting@163.com>
2025-12-26 21:39:25 +0800

    Add test code

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// 发送500测试用
+Route::get('/test-error', function () {
+    abort(500, 'Test 500 Error');
+});
+
 Route::get('/', function () {
     // 打印特定的环境变量（例如 APP_NAME 或自定义的）
     $envVar = env('DISPLAY_ENV_VAR', env('APP_NAME', 'Laravel'));
